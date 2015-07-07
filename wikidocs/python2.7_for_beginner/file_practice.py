f5226ad0cb08317c67ee704d11c0b82ef883401a
junggu.lee <junggu.lee@lge.com>
2015-07-07 12:09:59 +0900

    노래+가수 파일읽기

--- wikidocs/python2.7_for_beginner/file_practice.py
+++ wikidocs/python2.7_for_beginner/file_practice.py
@@ -14,6 +14,9 @@ print("TOTAL LENGTH : %d" % (len(text)))
 
 
 # 줄 단위로 작업하기
+
+## 문서 줄단위로 길이 출력
+
 oz_book = open("THE_WONDERFUL_WIZARD_OF_OZ.txt", 'r')
 text = oz_book.readlines()
 oz_book.close()
@@ -23,3 +26,20 @@ print("Total %d Line(s)" % (len(text)))
 for line in text:
 	print(len(line))
 
+## 파일 읽어 가수 + 노래 출력하기
+songs_file = open('songs.txt', 'r')
+num_of_songs = int(songs_file.readline())
+print('total songs : %d' % (num_of_songs))
+
+for num in range(num_of_songs):
+	singer = songs_file.readline()
+	song = songs_file.readline()
+
+	print("singer : %s" % singer)
+	print("song : %s" % song)
+
+
+
+## 파일읽어 사람 + 전화번호
+
+## 줄변 글자수, 전체 글자수 출력 
