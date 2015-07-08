4ea877b11d410a95f8d18a6c63c80dbb9ff50d2b
junggu.lee <junggu.lee@lge.com>
2015-07-08 09:29:14 +0900

    dict로만 구성

--- wikidocs/python2.7_for_beginner/phone_book.py
+++ wikidocs/python2.7_for_beginner/phone_book.py
@@ -9,21 +9,22 @@
 2. 파일에 결과 저장하기
 3. 파일에서 결과 읽어오기
 
+history
+ - 굳이 list를 사용해서 순서를 정할 필요가 있을까?
+   정렬을 해서 출력하면 되기 때문에 dict로 저장하는게 더 나아보인다.
+   구조도 더 단순해진다.
+
 """
 
 def display(a_phone_book):
 	print("NAME\tPHONE NUMBER")
-	for phone_info in a_phone_book:
-		for name, phone_num in phone_info.items():
-			print("%s\t%s" % (name, phone_num))
+	for name, phone_num in a_phone_book.items():
+		print("%s\t%s" % (name, phone_num))
 
-phone_book = []
+phone_book = {}
 
-phone_info1 = {'Jack' : '010-1234-1234'}
-phone_info2 = {'Hyde' : '02-111-1212'}
-
-phone_book.append(phone_info1)
-phone_book.append(phone_info2)
+phone_book['Jack'] = '010-1234-1234'
+phone_book['Hyde'] = '02-111-1212'
 
 display(phone_book)
 
